102dbca07ba3c8a181c19d6254f7ea3b87f94d2a
shaoyaming <sym@jfpal.com>
2015-11-29 22:38:33 +0800

    mm

--- app/src/main/java/com/example/sym/myapplication/ListViewActivity.java
+++ app/src/main/java/com/example/sym/myapplication/ListViewActivity.java
@@ -180,6 +180,9 @@ public class ListViewActivity extends Activity {
             return convertView;
         }
 
+
+
+
         private class ViewHolder{
             ImageView listview_item_iv;
             TextView listview_item_tv;
